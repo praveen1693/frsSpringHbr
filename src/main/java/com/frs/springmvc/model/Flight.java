0e28a1b8b07b72a9c3cbb5af037b7d971e89cbea
praveen1693 <52558109+praveen1693@users.noreply.github.com>
2019-08-21 12:09:43 +0530

    Update Flight.java

--- src/main/java/com/frs/springmvc/model/Flight.java
+++ src/main/java/com/frs/springmvc/model/Flight.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-@Table(name="Flight")
+@Table(name="flight")
 public class Flight {
 	
 	@Id
